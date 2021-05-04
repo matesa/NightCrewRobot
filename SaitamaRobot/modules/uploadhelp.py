ee12e97835add68556122289af177e43c56df041
matesa <75279583+matesa@users.noreply.github.com>
2021-05-04 21:43:00 +0300

    Create uploadhelp.py

---
+++ SaitamaRobot/modules/uploadhelp.py
@@ -0,0 +1,14 @@
+__mod_name__ = "upload"
+
+__help__ = """
+
+Upload modülü için yardım şu şekildedir:
+
+Dosyaya Bağlantı:
+ • /up: dosya olarak telgrafa yüklemek için doğrudan indirme bağlantısını yanıtla
+ 
+ Bağlanacak Dosya:
+ ⦁ /transfersh: aktarıcıya yüklemek için bir telegram dosyasını yanıtlayın ve doğrudan indirme bağlantısını alın
+ ⦁ /tmpninja: tmpninja'ya yüklemek için bir telegram dosyasını yanıtlayın ve doğrudan indirme bağlantısını alın
+ 
+ """
