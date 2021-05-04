e39f17b4909402eed4a5ed1cb2bdf2bfa37057a7
matesa <75279583+matesa@users.noreply.github.com>
2021-05-04 22:01:43 +0300

    Create tools.py

---
+++ SaitamaRobot/modules/tools.py
@@ -0,0 +1,23 @@
+__help__ = f"""
+*Mevcut Komutlar* *:*
+
+*Zipper*:
+• `/zip` *:* .zip biçiminde sıkıştırmak için bir telgraf dosyasını yanıtlayın
+• `/unzip` *:* bir telegram dosyasını .zip biçiminden açmak için yanıtlayın
+ 
+*QR*
+
+• `/getqr <resme yanıt ver>`*:* QR kodunu oku
+• `/makeqr <metne yanıtla>`*:* QR kodu oluştur
+ 
+*Görüntüler*
+• `/img` <resim adı>
+
+*IMDB*
+• `/imdb <film adı>
+
+*Quotly*
+• `/q - bot gibi alıntılar yapın`
+
+"""
+__mod_name__ = "Tools"
